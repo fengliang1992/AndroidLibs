3a4bb12057db4eb75cda13ee7dddd3cc88d30c6c
fengliang <732419012@qq.com>
2018-12-11 16:48:48 +0800

    二维码优化暂未完成

--- qrcode/src/main/java/com/fltry/module/qrcode/ZxingActivity.java
+++ qrcode/src/main/java/com/fltry/module/qrcode/ZxingActivity.java
@@ -1,15 +1,30 @@
 package com.fltry.module.qrcode;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.fltry.module.lib_common.AlertDialogUtils;
 import com.fltry.module.lib_common.BaseActivity;
 import com.fltry.module.qrcode.databinding.ActivityZxingBinding;
 import com.google.zxing.WriterException;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.bean.ZxingConfig;
+import com.yzq.zxinglibrary.common.Constant;
+
+import java.io.Serializable;
 
 
 public class ZxingActivity extends BaseActivity<ActivityZxingBinding> {
+    public static final int REQUEST_CODE_SCAN = 200;
+    final static int MY_PERMISSIONS_REQUEST_PHOTO = 2;
 
     @Override
     protected int getLayoutId() {
@@ -23,8 +38,55 @@ public class ZxingActivity extends BaseActivity<ActivityZxingBinding> {
 
     @Override
     protected void initView() {
+        if (Build.VERSION.SDK_INT >= 23
+                && getApplicationInfo().targetSdkVersion >= 23) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this
+                    , Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_PHOTO);
+                return;
+            }
+        }
+        dataBinding.zxingBtn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, CaptureActivity.class);
+                /*ZxingConfig是配置类
+                *可以设置是否显示底部布局，闪光灯，相册，
+                * 是否播放提示音  震动
+                * 设置扫描框颜色等
+                * 也可以不传这个参数
+                * */
+                ZxingConfig config = new ZxingConfig();
+                config.setPlayBeep(true);//是否播放扫描声音 默认为true
+                config.setShake(true);//是否震动  默认为true
+                config.setDecodeBarCode(true);//是否扫描条形码 默认为true
+                config.setReactColor(R.color.colorAccent);//设置扫描框四个角的颜色 默认为白色
+                config.setFrameLineColor(R.color.colorAccent);//设置扫描框边框颜色 默认无色
+                config.setScanLineColor(R.color.colorAccent);//设置扫描线的颜色 默认白色
+                config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
+                intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+                startActivityForResult(intent, REQUEST_CODE_SCAN);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                AlertDialogUtils.getMyAlert(mContext, "扫描结果", content);
+            }
+        }
 
     }
+
 
     public void clearPic(View v) {
         dataBinding.zxingEd1.setText("");
